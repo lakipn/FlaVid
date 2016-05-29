df55d1baec7bf9d9d143fa1490f73d51e13375d8
Lazar Milosavljević <lazar.milosavljevic.2318@metropolitan.ac.rs>
2016-05-29 20:46:31 +0200

    Routes for getting lists of fresh and popular videos in certain category.

--- api/api/controllers/CategoryController.js
+++ api/api/controllers/CategoryController.js
@@ -1,2 +1,33 @@
 module.exports = {
+  /**
+   * Get list of fresh videos in certain category
+   * @param request
+   * @param response
+     */
+  fresh : function(request, response) {
+    var catID = request.params['catId'];
+
+    var projection = { _id : 0, categories : { $elemMatch : { catId : catID } } };
+    Video.find({}, projection).sort("createdAt DESC").exec(function(err, res) {
+      if(err)
+        return response.serverError(err);
+      return response.ok(res);
+    });
+  },
+
+  /**
+   * Get list of most popular videos in certain category
+   * @param request
+   * @param response
+     */
+  popular : function(request, response) {
+    var catID = request.params['catId'];
+
+    var projection = { _id : 0, categories : { $elemMatch : { catId : catID } } };
+    Video.find({}, projection).sort("viewsCount DESC").exec(function(err, res) {
+      if(err)
+        return response.serverError(err);
+      return response.ok(res);
+    });
+  }
 };
