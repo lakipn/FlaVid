b5c3040b033aa6f2e705adc4916a7174e175aa25
Lazar Milosavljević <lazar.milosavljevic.2318@metropolitan.ac.rs>
2016-06-16 06:12:11 +0200

    Added routes: freshUsersVideos, uploadNewVideo & uploadedNewVideoDB

--- api/api/controllers/VideoController.js
+++ api/api/controllers/VideoController.js
@@ -5,11 +5,11 @@ module.exports = {
    * @param request
    * @param response
    */
-  populateCategories : function(request, response) {
+  populateCategories: function (request, response) {
     var vidID = request.param('vidId');
 
-    Video.find(vidID).populate('categories').exec(function(err, res) {
-      if(err)
+    Video.find(vidID).populate('categories').exec(function (err, res) {
+      if (err)
         return response.serverError(err);
       return response.json(res);
     });
@@ -19,12 +19,12 @@ module.exports = {
    * Populate video with it's comments
    * @param request
    * @param response
-     */
-  populateComments : function(request, response) {
+   */
+  populateComments: function (request, response) {
     var vidID = request.param('vidId');
 
-    Video.find(vidID).populate('comments').exec(function(err, res) {
-      if(err)
+    Video.find(vidID).populate('comments').exec(function (err, res) {
+      if (err)
         return response.serverError(err);
 
       return response.json(res);
@@ -35,12 +35,12 @@ module.exports = {
    * Populate video with it's likes
    * @param request
    * @param response
-     */
-  populateLikes : function(request, response) {
+   */
+  populateLikes: function (request, response) {
     var vidID = request.param('vidId');
 
-    Video.find(vidID).populate('likes').exec(function(err, res) {
-      if(err)
+    Video.find(vidID).populate('likes').exec(function (err, res) {
+      if (err)
         return response.serverError(err);
       return response.json(res);
     });
@@ -50,12 +50,12 @@ module.exports = {
    * Get META data about video
    * @param request
    * @param response
-     */
-  getMETA : function(request, response) {
+   */
+  getMETA: function (request, response) {
     var vidID = request.param('vidId');
 
-    Video.findOne(vidID).exec(function(err, res) {
-      if(err)
+    Video.findOne(vidID).exec(function (err, res) {
+      if (err)
         return response.serverError(err);
 
       return response.ok(res);
@@ -66,12 +66,12 @@ module.exports = {
    * Get statistical data about video
    * @param request
    * @param response
-     */
-  getStats : function(request, response) {
+   */
+  getStats: function (request, response) {
     var vidID = request.param('vidId');
 
-    Video.findOne(vidID).exec(function(err, res) {
-      if(err)
+    Video.findOne(vidID).exec(function (err, res) {
+      if (err)
         return response.serverError(err);
       var retObj = {};
       retObj.views = res['viewsCount'];
@@ -84,19 +84,19 @@ module.exports = {
    * Increment video's view count (every time a user see the video)
    * @param request
    * @param response
-     */
-  incrementViewsCount : function(request, response) {
+   */
+  incrementViewsCount: function (request, response) {
     var vidID = request.param('vidId');
 
     var currentCount;
-    Video.findOne(vidID).exec(function(err, res) {
-      if(err)
+    Video.findOne(vidID).exec(function (err, res) {
+      if (err)
         return response.serverError(err);
       currentCount = parseInt(res['viewsCount'], 10);
       currentCount++;
 
-      Video.update({ "id" : vidID }, { "viewsCount" : currentCount }).exec(function(err, res) {
-        if(err)
+      Video.update({"id": vidID}, {"viewsCount": currentCount}).exec(function (err, res) {
+        if (err)
           return response.serverError(err);
 
         return response.ok(res);
@@ -108,10 +108,10 @@ module.exports = {
    * Get list of most popular videos on network
    * @param request
    * @param response
-     */
-  popularOnNetwork : function(request, response) {
-    Video.find({}).sort("viewsCount DESC").exec(function(err, res) {
-      if(err)
+   */
+  popularOnNetwork: function (request, response) {
+    Video.find({}).sort("viewsCount DESC").exec(function (err, res) {
+      if (err)
         return response.serverError(err);
 
       return response.ok(res);
@@ -122,15 +122,72 @@ module.exports = {
    * Get list of fresh videos uploaded by followed users
    * @param request
    * @param response
-     */
-  freshFriends : function(request, response) {
+   */
+  freshFriends: function (request, response) {
     // NOT GOOD --- Should be: only videos uploaded by followed users
-    Video.find({}).sort("uploadedAt DESC").exec(function(err, res) {
-      if(err)
+    Video.find({}).sort("uploadedAt DESC").exec(function (err, res) {
+      if (err)
         return response.serverError(err);
 
       return response.ok(res);
     })
+  },
+
+  // DOVRSI!!!!
+  freshUsersVideos: function (request, response) {
+    Video.find({});
+  },
+
+  /**
+   * Upload new video file
+   * @param request
+   * @param response
+   * Important note: Not ready for production, because there's no file validation and compression!
+   */
+  uploadNewVideo: function (request, response) {
+    var filename = "";
+
+    request.file('video').upload({
+      // total file size should not exceed 500MB
+      maxBytes: 524288000,
+      dirname: require('path').resolve(sails.config.appPath, sails.config.appPath + '/assets/uploads/videos')
+    }, function (error, uploadedVideos) {
+      if (error)
+        return response.negotiate(error);
+
+      var str = uploadedVideos[0].fd;
+      var n = str.lastIndexOf("/");
+      var m = str.lastIndexOf(".");
+      filename = str.substr(n + 1, m-n - 1);
+
+      return response.json({
+        message: 'Successful uploaddd!',
+        data: {
+          filename : filename
+        }
+      });
+    });
+  },
+
+  /**
+   * "Inform" data base about new video
+   * @param request
+   * @param response
+     */
+  uploadNewVideoDB : function(request, response) {
+    console.log("Title: " + request.param('title') + "\t\tDuration: " + request.param('duration'));
+
+    Video.create({
+      title: request.param('title'),
+      duration: request.param('duration'),
+      sourceName : "FlaVid",
+      sourceUrl : '/assets/uploads/videos/' + request.param('filename'),
+      url : request.param('filename')
+    }).exec(function (err, created) {
+      if (err)
+        return response.serverError(err);
+      return response.data;
+    });
   }
 
 };
